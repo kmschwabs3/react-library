8bde2981957485e7ff810642dd60ddfef552b4a9
Krissy <35669945+kmschwabs3@users.noreply.github.com>
2019-07-08 20:19:22 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let bookList = [
+	{"title": "Pet Symetary", "author": "Stephen King", "pages": 300},
+	{"title": "Dead Wake", "author": "Erik Larson", "pages": 250},
+	{"title": "The Terror", "author": "Dan Simmons", "pages": 850},
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Child Component
+const Book = ({title, author, pages}) => {
+	return (
+		<section>
+			<h2>{title}</h2>
+			<p>By: {author}</p>
+			<p>Pages: {pages} pages</p>
+		</section>
+		)
+}
+
+//Parent Component
+class Library extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			open: true
+		}
+	}
+	render () {
+		const { books } = this.props
+		return (
+		<div>
+			<h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+			{books.map(
+				(book, i) => 
+						<Book 
+							key={i}
+							title={book.title} 
+							author={book.author} 
+							pages={book.pages}/>
+			)}
+			
+		</div>
+		)
+	}
+
+}
+
+
+render(
+		<Library books={bookList}/>,
+	document.getElementById('root')
+)
